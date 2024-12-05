39072d8dc8c1395aa1b371107370dd5296e0b533
farmage <farmage@protonmail.com>
2024-12-05 22:49:20 +0100

    bump to v.1.3.1_8

--- src/xcash_next/main.c
+++ src/xcash_next/main.c
@@ -15,7 +15,7 @@
 #include "VRF_functions.h"
 
 
-const char *argp_program_version = "Xcash Tech DPoPS v. 1.3.1_7";
+const char *argp_program_version = "Xcash Tech DPoPS v. 1.3.1_8";
 const char *argp_program_bug_address = "https://github.com/Xcash-Tech/xcash-tech-dpops/issues";
 
 static char doc[] = "Usage: xcash-dpops [OPTIONS]\n"
